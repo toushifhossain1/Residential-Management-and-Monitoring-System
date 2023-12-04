5b26459e07c5ac1c63c4a22cff840e1c0e2194f3
Nilufa Akter <nilufaakter234@gmail.com>
2023-12-04 15:14:45 +0600

    merge this one

---
+++ Web App/servant/servant.php
@@ -0,0 +1,269 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Sidebar 02</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+        integrity="sha384-..." crossorigin="anonymous"> <!-- Add the Font Awesome library -->
+</head>
+<style>
+    body,
+    html {
+        height: 100%;
+        margin: 0;
+        background-color: #ece6ff;
+
+    }
+
+    .container {
+        
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        color: #555;
+        /* Text color (grey) */
+    }
+
+    @media (max-width: 767px) {
+        .table-responsive {
+            overflow-x: auto;
+        }
+
+        .table-bordered {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        .table-bordered th,
+        .table-bordered td {
+            white-space: nowrap;
+        }
+    }
+
+    h3 {
+        color: blue;
+        /* Blue color for greeting */
+
+        margin-top: 30px;
+        font-weight: 400;
+    }
+
+    h1 {
+        color: blue;
+        /* Blue color for greeting */
+    }
+
+    .rules-container,
+
+    .servant-work-container {
+        border: 1px solid #ccc;
+        padding: 10px;
+        margin-top: 10px;
+        border-radius: 20px;
+        background-color: #f8f9fa;
+        /* Light gray background */
+    }
+
+    .complain-container .card {
+        margin-top: 10px;
+        background-color: #fff;
+        */
+    }
+
+    .servant-work-container {
+        border: 1px solid #ccc;
+        padding: 10px;
+        margin-top: 10px;
+        background-color: #f8f9fa;
+        /* Light gray background */
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        /* Center the content horizontally */
+        text-align: center;
+        /* Center the text within the container */
+    }
+
+    .clock-icon {
+        margin-right: 10px;
+        font-size: 24px;
+        /* Adjust the size of the clock icon */
+    }
+    button {
+    background-color: #bfbfbf;
+    color: black;
+    border-radius: 20px;
+    padding: 10px 20px;
+    border: none;
+    cursor: pointer;
+    margin: 0 auto;
+    display: block;
+    height: 50px;
+    width: 35%;
+    margin-top: 20px;
+}
+button:hover,
+button:active {
+    background-color: #d9d9d9; /* Change the background color on hover and click */
+    color: black; /* Change the text color on hover and click */
+}
+</style>
+
+<body>
+
+    <div class="wrapper d-flex align-items-stretch">
+        <nav id="sidebar">
+            <div class="custom-menu">
+                <button type="button" id="sidebarCollapse" class="btn btn-primary">
+                    <i class="fa fa-bars"></i>
+                    <span class="sr-only">Toggle Menu</span>
+                </button>
+            </div>
+            <div class="p-4 pt-5">
+                <h1><a href="index.html" class="logo">RMMS</a></h1>
+                <ul class="list-unstyled components mb-5">
+                    <li class="active">
+                        <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false"
+                            class="dropdown-toggle">Home</a>
+                        <ul class="collapse list-unstyled" id="homeSubmenu">
+                        
+                            <li>
+                                <a href="#">Update Information</a>
+                            </li>
+                            
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false"
+                            class="dropdown-toggle">Pages</a>
+                        <ul class="collapse list-unstyled" id="pageSubmenu">
+                            <li>
+                                <a href="#">Page 1</a>
+                            </li>
+                            <li>
+                                <a href="#">Page 2</a>
+                            </li>
+                            <li>
+                                <a href="#">Page 3</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fas fa-sign-out-alt"></i> Sign out
+                        </a>
+                    </li>
+                </ul>
+
+
+
+
+
+            </div>
+        </nav>
+
+        <!-- Page Content  -->
+        <div id="content" class="p-4 p-md-5 pt-5">
+            <div class="container">
+                <h1>Hi, User!</h1>
+
+
+
+                <h3><i class="fas fa-clock"></i> Upcoming Work Schedule</h3>
+
+                <div class="container">
+                    <div class="servant-work-container table-responsive">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Date</th>
+                                    <th scope="col">Flat Number</th>
+                                    <th scope="col">Time</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>2023-12-01</td>
+                                    <td>Flat 101</td>
+                                    <td>2.30PM</td>
+                                </tr>
+                                <tr>
+                                    <td>2023-12-01</td>
+                                    <td>Flat 101</td>
+                                    <td>2.30PM</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+
+
+                <!-- payment receive table -->
+
+
+                <h3><i class="fas fa-dollar-sign"></i> Payment received</h3>
+
+                <div class="container">
+                    <div class="servant-work-container table-responsive">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Date</th>
+                                    <th scope="col">Flat Number</th>
+                                    <th scope="col">Amount</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>2023-12-01</td>
+                                    <td>Flat 101</td>
+                                    <td>$1000</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+                
+                
+                    <button onclick="startTime()">Start Time</button>
+                    <button onclick="endTime()">End Time</button>
+                
+                
+            </div>
+
+
+
+            <script>
+        
+        function startTime() {
+            // Implement logic to handle the payment submission
+        }
+        function endTime() {
+            // Implement logic to handle the payment submission
+        }
+    </script>
+            <script src="path/to/bootstrap.js"></script>
+            <script src="js/jquery.min.js"></script>
+            <script src="js/popper.js"></script>
+            <script src="js/bootstrap.min.js"></script>
+            <script src="js/main.js"></script>
+
+</body>
+
+</html>
