583089c6fa0e75023d09d3e68f4cd4828bddb8f7
Nilufa Akter <nilufaakter234@gmail.com>
2023-12-05 13:05:56 +0600

    forms with alerts

--- Web App/rentee/submit_complain.php
+++ Web App/rentee/submit_complain.php
@@ -152,7 +152,7 @@ textarea.complaint-description-box {
           renter. Thank you for your help in improving our services.
         </p>
 
-        <form>
+        <form id="complaintForm">
         <div class="form-group">
   <b><label for="complaintDescription">Description of Complaint:</label></b>
   <textarea class="form-control" id="complaintDescription" rows="10" placeholder="Please describe your complaint in detail."></textarea>
@@ -181,7 +181,20 @@ textarea.complaint-description-box {
 
   
 </div>
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const form = document.getElementById('complaintForm');
+        form.addEventListener('submit', function (event) {
+            event.preventDefault(); // Prevent the default form submission
 
+            // Clear the form fields
+            form.querySelectorAll('input, textarea').forEach(element => element.value = '');
+
+            // Display an alert message
+            alert('Your complaint has been submitted successfully!');
+        });
+    });
+</script>
         <script src="js/jquery.min.js"></script>
         <script src="js/popper.js"></script>
         <script src="js/bootstrap.min.js"></script>
