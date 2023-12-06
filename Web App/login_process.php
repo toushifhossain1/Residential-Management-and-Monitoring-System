b2ad7286d49d0e8fb79809ed82852a3d0d2b0865
toushifhossain1 <1920404@iub.edu.bd>
2023-12-07 00:12:52 +0600

    Made the login page

--- Web App/login_process.php
+++ Web App/login_process.php
@@ -1,50 +1,48 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
     $UserID = $_POST['UserID'];
-    $Occupation = $_POST['Occupation'];
     $Password = $_POST['Password'];
-
-
 
     $link = mysqli_connect("localhost", "root", "", "rmms");
     if ($link === false) {
         die("ERROR: Could not connect. " . mysqli_connect_error());
     }
 
-    //from HTML
-    //echo "UserID: " . $UserID . "<br>";
-    //echo "Password: " . $Password . "<br>";
-    //echo "Occupation: " . $Occupation . "<br>";
-
-    $sql = "SELECT * FROM `user`;";
+    $sql = "SELECT * FROM `user` WHERE UserID = '$UserID' AND Password = '$Password';";
     $results = mysqli_query($link, $sql);
-    $row = mysqli_fetch_assoc($results);
-    //Since there is only one row in the database, therefore  only one row is fetched, for multiple rows run whileloop
-//while($row = mysqli_fetch_assoc($results);) {
-    // $row['attributeName'] will run through the whole tabvle
-//            }
 
-
-    //From Database
-    //echo '<br>';
-    //echo $row['UserID'] . "<br>";
-    //echo $row['Occupation'] . "<br>";
-    //echo $row['Password'] . "<br>";
-
-
-    if ($UserID == $row['UserID'] && $Occupation == $row['Occupation'] && $Password == $row['Password']) {
+    if (mysqli_num_rows($results) == 1) {
+        $row = mysqli_fetch_assoc($results);
         session_start();
-        $_SESSION['UserID'] = $UserID; // Set UserID in session -> this goes to the next page
-        //For Rentee
-        header("Location: rentee/rentee.php");
+        $_SESSION['UserID'] = $UserID;
+
+        switch ($row['Occupation']) {
+            case 'SelfLiving':
+                header("Location: flat_owner/flat_owner.php");
+                break;
+            case 'Renter':
+                header("Location: renter/renter.php");
+                break;
+            case 'Rentee':
+                header("Location: rentee/rentee.php");
+                break;
+            case 'Manager':
+                header("Location: manager/manager.php");
+                break;
+            case 'Guard':
+                header("Location: guard/guard.php");
+                break;
+            case 'Servant':
+                header("Location: servant/servant.php");
+                break;
+            default:
+                echo "Invalid Occupation";
+        }
     } else {
-        echo 'User id didnot match';
+        echo 'Invalid UserID or Password';
     }
 
-
+    mysqli_free_result($results);
     mysqli_close($link);
-
-
 }
 ?>
